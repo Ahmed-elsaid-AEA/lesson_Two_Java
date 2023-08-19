d4ad68fbbe33dd312bfd7f7f3c59ea74731721e8
ahmed-abmouty <ahmed122727727@gmail.com>
2023-08-19 17:55:06 +0300

    add my project

---
+++ src/lesson_two/Lesson_two.java
@@ -0,0 +1,120 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lesson_two;
+
+/**
+ *
+ * @author A.E.A
+ */
+public class Lesson_two {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+//      System.out.println("hello");
+        //int 
+        //String
+        //double
+        //char
+        //long
+//        double age = 10.5;
+        /*  double salary = 15.9;
+        int age =(int) salary;
+        salary = 20;
+        System.out.println("Age " + age);//null
+        System.out.println("salary " + salary);
+        String  a="ahmed";
+        a="mohamed";
+        System.out.println(a);*/
+        //+ add
+        //- sub
+        // / divi
+        // * mul
+        // % moduls
+//        System.out.println(1 + 5);//6
+//        System.out.println(1 * 5);//5
+//        System.out.println(1 - 5);//-4
+//        double a = (double) 1 / (double) 5;
+//        double x = 5;
+//        double y = 7;
+//        double b = (double)5 /(double)7;
+//        
+//
+//        double y = (double) 1 / (double) 2;
+//        double x = 1;
+//        double xx = 2;
+//        double yy = x / xx;
+//        System.out.println(yy);//
+
+//        System.out.println(10 / 5);
+//        System.out.println(2 % 5);
+//        System.out.println(15 % 4);
+//        double bb = (double) 20 / 100;
+//        System.out.println(bb * 100 + " %");
+//        
+        int a = 11;
+//        a = a + 1;
+//        a += 1;
+//        a++;
+
+//        a = a - 1;
+//        a -= 1;
+//        a--;
+//
+//        a = a / 2;
+//        a /= 2;
+//
+//        a = a * 2;
+//        a *= 2;
+//
+//        a = a % 2;
+//        a %= 2;
+//        a++;//post increment 12
+//        ++a;//pre incerement 12
+//   --a;//pre decrement 
+//   a--;post decrement
+//        System.out.println( a--);  
+//        System.out.println(a);
+//        int b=5;
+//        b--;
+//        System.out.println(b);
+//        =
+//                
+        int x;
+        double y, z;
+        z = 10;
+        x = 20;
+        y = 19;
+        double o = 20;
+        System.out.println(x == y);
+        System.out.println(x != y);
+        System.out.println(x > y);
+        System.out.println(x < y);
+        System.out.println(false && true);
+        System.out.println(x > z || x < y);
+
+//        true && true   = true
+//         false && true   = false
+//        true && false   = false
+//        false && false   = false  
+//        
+//           true || true   = true
+//         false || true   = true
+//        true || false   = true
+//        false || false   = false       
+        String name = "AHMEDHA ";
+        System.out.println(name.indexOf(65,2));
+        System.out.println(name.length());
+//        ASCII code
+//                a 95
+//                A 65
+        System.out.println(name.indexOf("A"));
+        
+        System.out.println(name.indexOf(65,2));
+        
+    }
+    
+}
